5a18f4aa6a3ee3bc645b7af36ea87d9f93289786
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-04-28 00:20:02 +0100

    feat&fix: fixed the core run_all script, and also added a cli

--- {{project_name_slug}}/core/__init__.py
+++ {{project_name_slug}}/core/__init__.py
@@ -4,12 +4,14 @@ def run_all():
     # Run all scripts in this folder in alphanumerical order
     scripts = list(f"{__package__}.{module_name}" for _, module_name, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
     scripts = sorted(scripts)
-    for s in scripts:
-        print(f"{'#' * (len(s)+4)}")
-        print(f"# {s} #")
-        print(f"{'#' * (len(s)+4)}")
-        if s in sys.modules:
-            module = importlib.import_module(s)
+    for script_full_import_path in scripts:
+        script_name = script_full_import_path.split(".")[-1]
+        if script_name.startswith("_"): continue
+        print(f"{'#' * (len(script_full_import_path)+4)}")
+        print(f"# {script_full_import_path} #")
+        print(f"{'#' * (len(script_full_import_path)+4)}")
+        if script_full_import_path in sys.modules:
+            module = importlib.import_module(script_full_import_path)
             importlib.reload(module)
         else:
-            importlib.import_module(s)
+            importlib.import_module(script_full_import_path)
